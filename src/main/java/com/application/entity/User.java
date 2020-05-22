ce614bfef237f577c3bf5e3619a2245960dd94e0
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-21 21:15:02 -0400

    Mapped entity to db

--- src/main/java/com/application/entity/User.java
+++ src/main/java/com/application/entity/User.java
@@ -1,8 +1,5 @@
 package com.application.entity;
 
-import com.application.entity.UserRole;
-import sun.util.resources.LocaleData;
-
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,15 +12,113 @@ public class User {
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
+    @Column(name = "name")
     private String name;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
     private String password;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+    @Column(name = "phone_number")
     private String phoneNumber;
+    @Column(name = "date_created")
     private LocalDateTime accountCreationDateTime;
-
     @ManyToOne
     @JoinColumn(name = "user_role_id")
     private UserRole userRole;
+
+    public User() {
+    }
+
+    public User(int userId, String name, String lastName, String email, String password, LocalDate birthDate, String phoneNumber, LocalDateTime accountCreationDateTime, UserRole userRole) {
+        this.userId = userId;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.accountCreationDateTime = accountCreationDateTime;
+        this.userRole = userRole;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getAccountCreationDateTime() {
+        return accountCreationDateTime;
+    }
+
+    public void setAccountCreationDateTime(LocalDateTime accountCreationDateTime) {
+        this.accountCreationDateTime = accountCreationDateTime;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + lastName + ": " + birthDate.toString();
+    }
 }
