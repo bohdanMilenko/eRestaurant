f1d90a01b930b4a06ed17b81a104a0b501e4ccc3
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-23 11:37:21 -0400

    Test run

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -1,4 +1,16 @@
 package com.application;
 
+import com.application.repository.IProvinceRepo;
+import com.application.service.IProvinceService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        IProvinceService provinceService = applicationContext.getBean(IProvinceService.class);
+
+        provinceService.findById(1);
+    }
 }
