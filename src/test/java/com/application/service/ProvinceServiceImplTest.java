1500d5d85c451ceebfe623dab2070439c6997fc2
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-27 23:06:02 -0400

    Created a test blueprint

---
+++ src/test/java/com/application/service/ProvinceServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.application.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProvinceServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllProvinces() {
+    }
+
+    @Test
+    void findByProvinceName() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByNameLike() {
+    }
+
+    @Test
+    void updateName() {
+    }
+}
