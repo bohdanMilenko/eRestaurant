81bbf8381611a157583fdd192d24570c769bdd31
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-29 18:54:27 -0400

    Changed exception handling in add()

--- src/main/java/com/application/repository/ProvinceRepoImpl.java
+++ src/main/java/com/application/repository/ProvinceRepoImpl.java
@@ -2,14 +2,13 @@ package com.application.repository;
 
 import com.application.entity.Province;
 import com.application.exception.EntityValidationException;
-import com.application.exception.NoSuchEntityException;
 import com.application.exception.RepoException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
-import java.util.Collection;
+import java.util.List;
 
 import static com.application.util.PassedEntitiesValidator.validateObjectsForNull;
 import static com.application.util.PassedEntitiesValidator.validateProvinceFieldsForNulls;
@@ -29,41 +28,45 @@ public class ProvinceRepoImpl implements IProvinceRepo {
     public void add(Province province) throws RepoException {
         try {
             validateObjectsForNull(province);
-            logger.info("Starting writing to DB by using add(province = {})", province.toString());
+            logger.info("Starting writing to DB by using add(province = {})", province);
             validateProvinceFieldsForNulls(province);
             em.persist(province);
+            logger.info("{} was successfully persisted by using add(province = (province))", province.toString());
         } catch (EntityValidationException e) {
-            logger.info("Object failed validation for add(province = {}))", province.toString());
+            logger.debug("Object failed validation for add(province = {}))", province);
             throw new RepoException("Validation for Province failed: " + province.toString(), e);
         } catch (PersistenceException e) {
-            logger.debug("{} violated table constraints and caused {}", province, e.toString());
+            logger.debug("{} violated table constraints and caused: {}", province, e.toString());
             throw new RepoException("{} failed to comply with the database constrains: " + province.toString()
                     + " .Cause " + e.getCause(), e);
         }
     }
 
     @Override
-    public Collection<Province> getAllProvinces() {
+    public List<Province> getAllProvinces() {
         TypedQuery<Province> query = em.createQuery("SELECT p FROM Province p", Province.class);
         return query.getResultList();
     }
 
     @Override
-    public Province findByString(String provinceToFind) throws RepoException {
+    public Province getByProvinceName(String provinceToFind) throws RepoException {
         logger.info("Entering findByString( provinceToFind = {}", provinceToFind);
         TypedQuery<Province> query = em.createQuery("SELECT p FROM Province p WHERE p.fullNameProvince =:province", Province.class)
                 .setParameter("province", provinceToFind);
         try {
             return query.getSingleResult();
         } catch (NoResultException e) {
-            logger.debug("Unable to find such record in table Province:" + provinceToFind, e);
-            throw new NoSuchEntityException("Province with name " + provinceToFind + " is not found", e);
+            logger.debug("Unable to find such record in table Province {}, cause: {}", provinceToFind, e.toString());
+            throw new RepoException("Province with name " + provinceToFind + " is not found", e);
+        } catch (NonUniqueResultException e) {
+            logger.debug("Multiple results found for passed : {}, cause: {}", provinceToFind, e.toString());
+            throw new RepoException("Multiple results found or passed : " + provinceToFind, e);
         }
     }
 
     @Override
-    public Collection<Province> findByNameLike(String provinceToFind) {
-        logger.info("Entering findByNameLike( provinceToFind = {}", provinceToFind);
+    public List<Province> getByNameLike(String provinceToFind) {
+        logger.info("Entering findByNameLike(provinceToFind = {})", provinceToFind);
         TypedQuery<Province> query = em.createQuery("SELECT p FROM Province p WHERE p.fullNameProvince LIKE :province " +
                 "ORDER BY p.fullNameProvince", Province.class)
                 .setParameter("province", provinceToFind + "%");
@@ -73,7 +76,7 @@ public class ProvinceRepoImpl implements IProvinceRepo {
 
 
     @Override
-    public Province findById(int id) {
+    public Province getById(int id) {
         return em.find(Province.class, id);
     }
 
@@ -83,17 +86,14 @@ public class ProvinceRepoImpl implements IProvinceRepo {
             validateObjectsForNull(oldProvince, newProvince);
             validateProvinceFieldsForNulls(oldProvince, newProvince);
             logger.info("Entering updateName( old = {}, new = {} )", oldProvince, newProvince);
-            if (oldProvince.getFullNameProvince() != null && newProvince.getFullNameProvince() != null) {
-
-                Province province = findByString(oldProvince.getFullNameProvince());
-                province.setFullNameProvince(newProvince.getFullNameProvince());
-                logger.info("Successfully update provinces old = {}, new = {} )", oldProvince, newProvince);
-                return true;
-            }
+            validateProvinceFieldsForNulls(oldProvince, newProvince);
+            Province province = getByProvinceName(oldProvince.getFullNameProvince());
+            province.setFullNameProvince(newProvince.getFullNameProvince());
+            logger.info("Successfully update provinces old = {}, new = {} )", oldProvince, newProvince);
+            return true;
         } catch (EntityValidationException e) {
             logger.debug("Passed entities failed validation: updateName( old = {}, new ={})", oldProvince, newProvince);
             throw new RepoException(e);
         }
-        return false;
     }
 }
