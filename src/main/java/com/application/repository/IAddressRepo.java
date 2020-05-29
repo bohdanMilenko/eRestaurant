3aeafe66c1d4d0d68166b80c81fed621fe3a5539
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-29 18:53:02 -0400

    Created blueprint for Address

--- src/main/java/com/application/repository/IAddressRepo.java
+++ src/main/java/com/application/repository/IAddressRepo.java
@@ -1,4 +1,21 @@
 package com.application.repository;
 
+import com.application.entity.Address;
+import com.application.entity.User;
+
+import java.util.List;
+
 public interface IAddressRepo {
+
+    void addAddress(User user, Address address);
+
+    Address getAddressById(int id);
+
+    //TODO - IS GETTING ADDRESS User's scope or Address scope?
+    List<Address> getAddressByUserId(int userId);
+
+    boolean updateAddress(int userId, Address oldAddress, Address newAddress);
+
+    boolean removeAddress(Address address, User user);
+
 }
