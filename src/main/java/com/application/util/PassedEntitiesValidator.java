691207a28b37e7c3dda99e5095315738159c0c15
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-28 23:01:05 -0400

    New overloaded methods

--- src/main/java/com/application/util/PassedEntitiesValidator.java
+++ src/main/java/com/application/util/PassedEntitiesValidator.java
@@ -1,5 +1,6 @@
 package com.application.util;
 
+import com.application.entity.Province;
 import com.application.entity.UserRole;
 import com.application.exception.EntityValidationException;
 
@@ -13,19 +14,37 @@ public class PassedEntitiesValidator {
 
     public static void validateObjectsForNull(Object o1, Object o2) throws EntityValidationException {
         if (o1 == null || o2 == null) {
-            throw new EntityValidationException("Objects are null");
+            throw new EntityValidationException("o1 is " + o1 + " o2 is" + o2);
         }
     }
 
     public static void validateObjectsForNull(Object o1, Object o2, Object o3) throws EntityValidationException {
         if (o1 == null || o2 == null || o3 == null) {
-            throw new EntityValidationException("Objects are null");
+            throw new EntityValidationException("o1 is " + o1 + " o2 is" + o2+ " o3 is" + o3);
         }
     }
 
     public static void validateUserRoleFieldsForNulls(UserRole o1) throws EntityValidationException {
         if (o1.getRoleName() == null) {
             throw new EntityValidationException("UserRole name is null: " + o1.toString());
+        }
+    }
+    public static void validateUserRoleFieldsForNulls(UserRole o1, UserRole o2) throws EntityValidationException {
+        if (o1.getRoleName() == null) {
+            throw new EntityValidationException("UserRole name is null \no1: " + o1.toString() + "\no2: " + o2.toString());
+        }
+    }
+
+    public static void validateProvinceFieldsForNulls(Province o1) throws EntityValidationException {
+        if (o1.getFullNameProvince() == null || o1.getAbbreviationProvince()==null) {
+            throw new EntityValidationException("Province fields contain nulls: " + o1.toString());
+        }
+    }
+
+    public static void validateProvinceFieldsForNulls(Province o1, Province o2) throws EntityValidationException {
+        if (o1.getFullNameProvince() == null || o1.getAbbreviationProvince()==null) {
+            throw new EntityValidationException("Province fields contain nulls \no1: " + o1.toString() +
+                    "\no2 :" + o2.toString());
         }
     }
 
