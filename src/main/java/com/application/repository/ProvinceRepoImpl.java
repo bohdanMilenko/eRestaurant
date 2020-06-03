179f90bab2488e05a83cf460090ac1755a4c97a8
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-02 23:18:23 -0400

    Moved logging and null validation to service

--- src/main/java/com/application/repository/ProvinceRepoImpl.java
+++ src/main/java/com/application/repository/ProvinceRepoImpl.java
@@ -27,14 +27,8 @@ public class ProvinceRepoImpl implements IProvinceRepo {
     @Override
     public void add(Province province) throws RepoException {
         try {
-            validateObjectsForNull(province);
-            logger.info("Starting writing to DB by using add(province = {})", province);
-            validateProvinceFieldsForNulls(province);
             em.persist(province);
             logger.info("{} was successfully persisted by using add(province = (province))", province.toString());
-        } catch (EntityValidationException e) {
-            logger.error("Object failed validation for add(province = {}))", province);
-            throw new RepoException("Validation for Province failed: " + province.toString(), e);
         } catch (PersistenceException e) {
             logger.error("{} violated table constraints and caused: {}", province, e.toString());
             throw new RepoException("{} failed to comply with the database constrains: " + province.toString()
@@ -50,23 +44,21 @@ public class ProvinceRepoImpl implements IProvinceRepo {
 
     @Override
     public Province getByProvinceName(String provinceToFind) throws RepoException {
-        logger.info("Entering findByString( provinceToFind = {}", provinceToFind);
         TypedQuery<Province> query = em.createQuery("SELECT p FROM Province p WHERE p.fullNameProvince =:province", Province.class)
                 .setParameter("province", provinceToFind);
         try {
             return query.getSingleResult();
         } catch (NoResultException e) {
-            logger.debug("Unable to find such record in table Province {}, cause: {}", provinceToFind, e.toString());
+            logger.error("Unable to find such record in table Province {}, cause: {}", provinceToFind, e.toString());
             throw new RepoException("Province with name " + provinceToFind + " is not found", e);
         } catch (NonUniqueResultException e) {
-            logger.debug("Multiple results found for passed : {}, cause: {}", provinceToFind, e.toString());
-            throw new RepoException("Multiple results found or passed : " + provinceToFind, e);
+            logger.error("Multiple results found for passed : {}, cause: {}", provinceToFind, e.toString());
+            throw new RepoException("Multiple results found for passed : " + provinceToFind, e);
         }
     }
 
     @Override
     public List<Province> getByNameLike(String provinceToFind) {
-        logger.info("Entering findByNameLike(provinceToFind = {})", provinceToFind);
         TypedQuery<Province> query = em.createQuery("SELECT p FROM Province p WHERE p.fullNameProvince LIKE :province " +
                 "ORDER BY p.fullNameProvince", Province.class)
                 .setParameter("province", provinceToFind + "%");
@@ -82,18 +74,10 @@ public class ProvinceRepoImpl implements IProvinceRepo {
 
     @Override
     public boolean updateName(Province oldProvince, Province newProvince) throws RepoException {
-        try {
-            validateObjectsForNull(oldProvince, newProvince);
-            validateProvinceFieldsForNulls(oldProvince, newProvince);
-            logger.info("Entering updateName( old = {}, new = {} )", oldProvince, newProvince);
-            validateProvinceFieldsForNulls(oldProvince, newProvince);
             Province province = getByProvinceName(oldProvince.getFullNameProvince());
             province.setFullNameProvince(newProvince.getFullNameProvince());
             logger.info("Successfully update provinces old = {}, new = {} )", oldProvince, newProvince);
             return true;
-        } catch (EntityValidationException e) {
-            logger.debug("Passed entities failed validation: updateName( old = {}, new ={})", oldProvince, newProvince);
-            throw new RepoException(e);
-        }
+
     }
 }
