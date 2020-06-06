805d76f23d2fa2c2e381f8fe17e1a479a655ed24
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-05 23:32:23 -0400

    Added passed objects validation and logging

--- src/main/java/com/application/repository/UserRoleRepoImpl.java
+++ src/main/java/com/application/repository/UserRoleRepoImpl.java
@@ -1,8 +1,6 @@
 package com.application.repository;
 
 import com.application.entity.UserRole;
-import com.application.exception.DuplicatedEntityInDbException;
-import com.application.exception.EntityValidationException;
 import com.application.exception.RepoException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +9,6 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.*;
 import javax.transaction.Transactional;
 import java.util.List;
-
-import static com.application.util.PassedEntitiesValidator.*;
 
 @Repository
 public class UserRoleRepoImpl implements IUserRoleRepo {
@@ -24,15 +20,9 @@ public class UserRoleRepoImpl implements IUserRoleRepo {
     @Override
     public void addRole(UserRole role) throws RepoException {
         try {
-            logger.info("Starting writing to DB, addRole( role = {})", role);
-            validateObjectsForNull(role);
-            validateUserRoleFieldsForNulls(role);
-                em.persist(role);
-        }catch (EntityValidationException e){
-            logger.debug("Object failed validation for add(province = {}))", role);
-            throw new RepoException("Invalid passed argument. Unable to add new UserRole "+ role, e);
-        }catch (PersistenceException e){
-            logger.debug("Violation of UserRepo table constraints: addRole(role={}) caused: {}", role, e.toString());
+            em.persist(role);
+        } catch (PersistenceException e) {
+            logger.error("Violation of UserRepo table constraints: addRole(role={}) caused: {}", role, e.toString());
             throw new RepoException("Such Entity already exists in DB: " + role.getRoleName(), e);
         }
     }
@@ -46,76 +36,51 @@ public class UserRoleRepoImpl implements IUserRoleRepo {
 
     @Override
     public UserRole getByRoleName(String roleName) throws RepoException {
-            try {
-                validateObjectsForNull(roleName);
-                TypedQuery<UserRole> query = em.createQuery("SELECT u FROM UserRole u WHERE u.roleName = :roleName", UserRole.class);
-                query.setParameter("roleName", roleName);
-                logger.info("Found record: " +query.getSingleResult().toString(), query.getSingleResult() );
-                return query.getSingleResult();
-            }catch (EntityValidationException e) {
-                logger.debug("Passed argument failed validation getByRoleName( roleName = {}), cause: {}",roleName, e);
-                throw new RepoException("Failed validation",e);
-            }
-            catch (NonUniqueResultException e){
-                logger.debug("Multiple results found for passed : {}, cause: {}",roleName, e.toString());
-                return null;
-            }catch (NoResultException e){
-                logger.debug("Unable to find such record in table UserName {}, cause: {}",roleName, e.toString());
-                return null;
-            }
-        }
-
-
-    @Override
-    public List<UserRole> getByUserRoleNameLike(String roleName) throws RepoException {
         try {
-            validateObjectsForNull(roleName);
-            TypedQuery<UserRole> query = em.createQuery("SELECT u FROM UserRole u WHERE u.roleName LIKE :roleName", UserRole.class);
-            query.setParameter("roleName", roleName +"%");
-            return query.getResultList();
-        }
-        catch (EntityValidationException e) {
-            logger.debug("Passed argument failed validation getByRoleName( roleName = {}), cause: {}",roleName, e);
-            throw new RepoException("Failed validation",e);
+            TypedQuery<UserRole> query = em.createQuery("SELECT u FROM UserRole u WHERE u.roleName = :roleName", UserRole.class);
+            query.setParameter("roleName", roleName);
+            logger.info("Found record: " + query.getSingleResult().toString(), query.getSingleResult());
+            return query.getSingleResult();
+        } catch (NonUniqueResultException e) {
+            logger.error("Multiple results found for passed : {}, cause: {}", roleName, e.toString());
+            return null;
+        } catch (NoResultException e) {
+            logger.error("Unable to find such record in table UserName {}, cause: {}", roleName, e.toString());
+            return null;
         }
     }
 
 
     @Override
+    public List<UserRole> getByUserRoleNameLike(String roleName) throws RepoException {
+        TypedQuery<UserRole> query = em.createQuery("SELECT u FROM UserRole u WHERE u.roleName LIKE :roleName", UserRole.class);
+        query.setParameter("roleName", roleName + "%");
+        return query.getResultList();
+
+    }
+
+
+    @Override
     public boolean updateName(UserRole role, UserRole newUserRole) throws RepoException {
-        try {
-            logger.info("Entering updateName( old = {}, new = {} )", role, newUserRole);
-            validateObjectsForNull(role, newUserRole);
-            validateUserRoleFieldsForNulls(role, newUserRole);
-            UserRole oldUserRole = getByRoleName(role.getRoleName());
-            oldUserRole.setRoleName(newUserRole.getRoleName());
-            logger.info("Successfully updated roleNames old = {}, new = {}", role, newUserRole);
-            return true;
-        }catch (EntityValidationException e){
-            //TODO - Logging
-            //What level of logging do I need when catching exception?
-            logger.debug("Failed validation while updating UserRoles: old = {}, new = {}, cause: {}", role, newUserRole, e.toString());
-            throw new RepoException(e);
-        }
+        logger.info("Entering updateName( old = {}, new = {} )", role, newUserRole);
+        UserRole oldUserRole = getByRoleName(role.getRoleName());
+        oldUserRole.setRoleName(newUserRole.getRoleName());
+        logger.info("Successfully updated roleNames old = {}, new = {}", role, newUserRole);
+        return true;
+
     }
 
 
     @Override
     @Transactional
     public boolean remove(UserRole role) throws RepoException {
-        try {
-            logger.info("Entering remove( userRole = {})", role);
-            validateObjectsForNull(role);
-            validateUserRoleFieldsForNulls(role);
-            UserRole userRole = getByRoleName(role.getRoleName());
-            Query query = em.createQuery("DELETE FROM UserRole ur where ur.roleName IN :roleName");
-            query.setParameter("roleName", userRole.getRoleName());
-            logger.info("Removed from DB userRole = {})", role);
-            query.executeUpdate();
-            return true;
-        }catch (EntityValidationException e){
-            logger.debug("{} failed validation for nulls", role);
-            throw new RepoException(e);
-        }
+        UserRole userRole = getByRoleName(role.getRoleName());
+        Query query = em.createQuery("DELETE FROM UserRole ur where ur.roleName IN :roleName");
+        query.setParameter("roleName", userRole.getRoleName());
+        logger.info("Removed from DB userRole = {})", role);
+        query.executeUpdate();
+        return true;
+
+
     }
 }
--- src/main/java/com/application/service/UserRoleServiceImpl.java
+++ src/main/java/com/application/service/UserRoleServiceImpl.java
@@ -1,19 +1,27 @@
 package com.application.service;
 
 import com.application.entity.UserRole;
+import com.application.exception.EntityValidationException;
 import com.application.exception.RepoException;
 import com.application.exception.ServiceException;
 import com.application.repository.IUserRoleRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+import static com.application.util.PassedEntitiesValidator.validateObjectsForNull;
+import static com.application.util.PassedEntitiesValidator.validateUserRoleFieldsForNulls;
+
 @Service
 public class UserRoleServiceImpl implements IUserRoleService {
     @Autowired
     IUserRoleRepo userRoleRepo;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserRoleServiceImpl.class);
 
     public UserRoleServiceImpl() {
     }
@@ -25,11 +33,18 @@ public class UserRoleServiceImpl implements IUserRoleService {
     @Override
     @Transactional
     public void add(UserRole userRole) throws ServiceException {
-       try{
-           userRoleRepo.addRole(userRole);
-       }catch (RepoException e){
-           throw new ServiceException("Unable to add new role, validation failed", e);
-       }
+        logger.info("Starting writing to DB, add( userRole = {})", userRole);
+        try {
+            validateObjectsForNull(userRole);
+            validateUserRoleFieldsForNulls(userRole);
+            userRoleRepo.addRole(userRole);
+        } catch (EntityValidationException e) {
+            logger.debug("Object failed validation for add(province = {}))", userRole);
+            throw new ServiceException("Invalid passed argument. Unable to add new UserRole " + userRole, e);
+        } catch (RepoException e) {
+            logger.error("Unable to find add( userRole = {}), as it caused: {}", userRole, e.toString());
+            throw new ServiceException("Unable to add new role, validation failed", e);
+        }
     }
 
     @Override
@@ -39,42 +54,61 @@ public class UserRoleServiceImpl implements IUserRoleService {
 
     @Override
     public UserRole getByRoleName(UserRole userRole) throws ServiceException {
-        //TODO - Validation for Nulls
-        //I need to check here if the object is null and if the name is null, but do
-        // I need validation on both Service and Repo levels?
-        return userRoleRepo.getByRoleName(userRole.getRoleName());
-    }
-
-    @Override
-    public List<UserRole> getByUserRoleNameLike(UserRole roleName) throws ServiceException {
-        //TODO - Validation for Nulls
-        //I need to check here if the object is null and if the name is null, but do
-        // I need validation on both Service and Repo levels?
+        logger.info("Queering by using  getByRoleName( userRole = {})", userRole);
         try {
-            return userRoleRepo.getByUserRoleNameLike(roleName.getRoleName());
-        }catch (RepoException e){
-            throw new ServiceException("Repo failed to get UserRole by: " + roleName, e);
+            validateObjectsForNull(userRole);
+            return userRoleRepo.getByRoleName(userRole.getRoleName());
+        } catch (EntityValidationException e) {
+            logger.error("Passed argument failed validation getByRoleName( userRole = {}), cause: {}", userRole, e);
+            throw new RepoException("Failed validation", e);
         }
     }
 
     @Override
-    public boolean updateName(UserRole role, UserRole newUserRole) throws ServiceException {
-        try{
-            userRoleRepo.updateName(role, newUserRole);
-        }catch (RepoException e){
-            //TODO do I need logging here as well as for the same method in Repo? which level of logging
-            throw new ServiceException("Failed to update UserRole. Passed arguments failed validation:" +
-                    "\nOld UserRole: " + role.toString() + "\nNew UserRole: " + newUserRole.toString(),e);
+    public List<UserRole> getByUserRoleNameLike(UserRole userRole) throws ServiceException {
+        logger.info("Queering by using getByUserRoleNameLike( userRole = {})", userRole);
+        try {
+            validateObjectsForNull(userRole);
+            return userRoleRepo.getByUserRoleNameLike(userRole.getRoleName());
+        } catch (EntityValidationException e) {
+            logger.error("Passed argument failed validation getByRoleName( roleName = {}), cause: {}", userRole, e);
+            throw new RepoException("Failed validation", e);
+        } catch (RepoException e) {
+            logger.error("Unable to find getByUserRoleNameLike( userRole = {}), as it caused: {}", userRole, e.toString());
+            throw new ServiceException("Repo failed to get UserRole by: " + userRole, e);
+        }
+    }
+
+    @Override
+    public boolean updateName(UserRole userRole, UserRole newUserRole) throws ServiceException {
+        logger.info("Starting changing records in DB with updateName( userRole = {})", userRole);
+        try {
+            validateObjectsForNull(userRole, newUserRole);
+            validateUserRoleFieldsForNulls(userRole, newUserRole);
+            userRoleRepo.updateName(userRole, newUserRole);
+        } catch (EntityValidationException e) {
+            logger.error("Failed validation while updating UserRoles: old = {}, new = {}, cause: {}", userRole, newUserRole, e.toString());
+            throw new RepoException(e);
+        } catch (RepoException e) {
+            logger.error("Unable to find updateName( userRole = {}), as it caused: {}", userRole, e.toString());
+            throw new ServiceException("Repo failed to update UserRole: " + userRole.toString() + ", " + newUserRole.toString(), e);
         }
         return false;
     }
 
     @Override
-    public boolean remove(UserRole role) throws ServiceException {
+    public boolean remove(UserRole userRole) throws ServiceException {
+        logger.info("Entering remove( userRole = {})", userRole);
         try {
-            return userRoleRepo.remove(role);
-        }catch (RepoException e){
-            throw new ServiceException("Cannot remove user: " + role.toString(), e);
+            validateObjectsForNull(userRole);
+            validateUserRoleFieldsForNulls(userRole);
+            return userRoleRepo.remove(userRole);
+        } catch (EntityValidationException e) {
+            logger.error("{} failed validation for nulls", userRole);
+            throw new RepoException(e);
+        } catch (RepoException e) {
+            logger.error("Unable to find remove( userRole = {}), as it caused: {}", userRole, e.toString());
+            throw new ServiceException("Repo cannot remove userRole: " + userRole.toString(), e);
         }
     }
 }
