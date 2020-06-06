e5a4648f74300212c5250d82ec0def89d8ed2b00
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-06 00:13:54 -0400

    Finished entity creation

--- src/main/java/com/application/entity/Order.java
+++ src/main/java/com/application/entity/Order.java
@@ -1,6 +1,7 @@
 package com.application.entity;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "order")
@@ -10,5 +11,63 @@ public class Order {
     @Column(name = "order_id")
     private int order_id;
 
-    //TODO - FINISH
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "order_status_id")
+    private OrderStatus orderStatus;
+    @Column(name = "ordered_time")
+    private LocalDateTime orderedTime;
+
+    public Order() {
+    }
+
+    public Order(User user, OrderStatus orderStatus, LocalDateTime orderedTime) {
+        this.user = user;
+        this.orderStatus = orderStatus;
+        this.orderedTime = orderedTime;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public LocalDateTime getOrderedTime() {
+        return orderedTime;
+    }
+
+    public void setOrderedTime(LocalDateTime orderedTime) {
+        this.orderedTime = orderedTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "order_id=" + order_id +
+                ", user=" + user +
+                ", orderStatus=" + orderStatus +
+                ", orderedTime=" + orderedTime +
+                '}';
+    }
 }
