2346559c9e9e57185e39e24b96e4ded29a6aeeb6
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-07 22:54:38 -0400

    Formatting fix

--- src/main/java/com/application/service/AddressServiceImpl.java
+++ src/main/java/com/application/service/AddressServiceImpl.java
@@ -8,10 +8,12 @@ import com.application.repository.IAddressRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 import static com.application.util.PassedEntitiesValidator.validateAddressFieldsForNulls;
 import static com.application.util.PassedEntitiesValidator.validateObjectsForNull;
@@ -19,14 +21,13 @@ import static com.application.util.PassedEntitiesValidator.validateObjectsForNul
 @Service
 public class AddressServiceImpl implements IAddressService {
 
-    @Autowired
     private IAddressRepo addressRepo;
-
     private static final Logger logger = LoggerFactory.getLogger(AddressServiceImpl.class);
 
     public AddressServiceImpl() {
     }
 
+    @Autowired
     public AddressServiceImpl(IAddressRepo addressRepo) {
         this.addressRepo = addressRepo;
     }
@@ -40,8 +41,8 @@ public class AddressServiceImpl implements IAddressService {
             validateAddressFieldsForNulls(address);
             validateObjectsForNull(address.getUser());
             validateObjectsForNull(address.getUser().getUserId());
-            if (addressRepo.getByAddressAndUserId(address.getUser().getUserId(), address.getAddressLine1()) == null) {
-                addressRepo.addAddress(address);
+            if (addressRepo.getByUserIdAndAddressLine1(address.getUser().getUserId(), address.getAddressLine1()) == null) {
+                addressRepo.save(address);
             } else {
                 logger.error("Unable to execute add (address = {}), duplicated address", address);
                 throw new ServiceException("Attempt to add duplicated address for user: " + address.getUser().getUserId());
@@ -50,7 +51,7 @@ public class AddressServiceImpl implements IAddressService {
         } catch (EntityValidationException e) {
             logger.error("Object failed validation for add(address = {}))", address);
             throw new ServiceException("Passed entity failed validation: " + address, e);
-        }catch (RepoException e) {
+        } catch (RepoException e) {
             logger.error("Repo threw exception while add( address = {}, and caused: {}", address, e.toString());
             throw new ServiceException("Repo failed to add new Address: " + address.toString(), e);
         }
@@ -58,14 +59,14 @@ public class AddressServiceImpl implements IAddressService {
 
     @Override
     public List<Address> getAllAddresses() {
-        return addressRepo.getAllAddresses();
+        return addressRepo.findAll();
     }
 
     @Override
-    public Address getAddressById(int id) throws ServiceException {
+    public Optional<Address> getAddressById(int id) throws ServiceException {
         try {
             validateObjectsForNull(id);
-            return addressRepo.getAddressById(id);
+            return addressRepo.findById(id);
         } catch (EntityValidationException e) {
             logger.error("Object failed validation for getAddressById(id = {}))", id);
             throw new ServiceException("Passed entity failed validation: " + id, e);
@@ -76,7 +77,7 @@ public class AddressServiceImpl implements IAddressService {
     public List<Address> getAddressByUserId(int userId) throws ServiceException {
         try {
             validateObjectsForNull(userId);
-            return addressRepo.getAddressByUserId(userId);
+            return addressRepo.getAddressesByUserAndSort(userId, Sort.by(Sort.Direction.DESC, "addressLine1"));
         } catch (EntityValidationException e) {
             logger.error("Object failed validation for getAddressByUserId(id = {}))", userId);
             throw new ServiceException("Passed entity failed validation: " + userId, e);
@@ -85,13 +86,18 @@ public class AddressServiceImpl implements IAddressService {
 
     @Override
     @Transactional
-    public boolean updateAddress(int oldAddressId, Address updatedAddress) throws ServiceException {
+    public Address updateAddress(int oldAddressId, Address updatedAddress) throws ServiceException {
         logger.info("Entering updateAddress( oldAddressId = {}, updatedAddress = {} )", oldAddressId, updatedAddress);
         try {
             validateObjectsForNull(oldAddressId);
             validateObjectsForNull(updatedAddress);
             validateAddressFieldsForNulls(updatedAddress);
-            return addressRepo.updateAddress(oldAddressId, updatedAddress);
+            Optional<Address> fetchedAddress = getAddressById(oldAddressId);
+            if (fetchedAddress.isPresent()) {
+                updatedAddress.setAddressId(oldAddressId);
+                return addressRepo.save(updatedAddress);
+            }
+            return null;
         } catch (EntityValidationException e) {
             logger.error("Objects failed validation for updateAddress(oldAddressId = {}," +
                     " updatedAddress = {}), cause: {} ", oldAddressId, updatedAddress, e.toString());
@@ -101,11 +107,11 @@ public class AddressServiceImpl implements IAddressService {
 
     @Override
     @Transactional
-    public boolean removeAddress(Address address) throws ServiceException {
+    public void removeAddress(Address address) throws ServiceException {
         try {
             validateObjectsForNull(address);
             validateAddressFieldsForNulls(address);
-            return addressRepo.removeAddress(address);
+            addressRepo.delete(address);
         } catch (EntityValidationException e) {
             logger.error("Object failed validation for getAddressByUserId(id = {}))", address);
             throw new ServiceException("Passed entity failed validation: " + address, e);
