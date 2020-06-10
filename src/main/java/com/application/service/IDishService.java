fcd3a4b27d70b0633958131709ebcfbe9ae2d8c0
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-09 23:43:00 -0400

    Implemented IDishService interface

--- src/main/java/com/application/service/IDishService.java
+++ src/main/java/com/application/service/IDishService.java
@@ -1,4 +1,42 @@
 package com.application.service;
 
+import com.application.entity.Dish;
+import com.application.entity.MenuItem;
+import com.application.entity.Order;
+import com.application.entity.dto.PopularDishReport;
+import com.application.exception.ServiceException;
+
+import java.util.List;
+
+
 public interface IDishService {
+
+//    void addDish(Dish dish) throws ServiceException;
+
+    void addDishes(List<Dish> dishes) throws ServiceException;
+
+    List<Dish> getAllDishes();
+
+    List<Dish> getDishesByOrder(Order order) throws ServiceException;
+
+    List<Dish> getDishesByMenuItem(MenuItem menuItem) throws ServiceException;
+
+    List<Dish> getDishesByOrderAndMenuItem(Order order, MenuItem menuItem) throws ServiceException;
+
+    int getSumByDish(Dish dish);
+
+    int getSumByOrder(Order order);
+
+    void moveDishOneStatusFurther(Dish dish);
+
+    boolean updateQuantity(Dish dish);
+
+    List<PopularDishReport> generatePopularDishReport();
+
+    void removeDish(Dish dish);
+
+
+
+
+
 }
