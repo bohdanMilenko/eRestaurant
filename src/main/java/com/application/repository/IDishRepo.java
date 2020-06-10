44c025af497a13883f38bd5729374a310c563c5e
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-09 21:03:37 -0400

    Created a repo for Dish

---
+++ src/main/java/com/application/repository/IDishRepo.java
@@ -0,0 +1,32 @@
+package com.application.repository;
+
+import com.application.entity.Dish;
+import com.application.entity.DishStatus;
+import com.application.entity.MenuItem;
+import com.application.entity.Order;
+import com.application.entity.dto.PopularDishReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface IDishRepo extends JpaRepository<Dish, Integer> {
+
+    List<Dish> getDishesByDishStatus(DishStatus dishStatus);
+
+    List<Dish> getDishesByOrder(Order order);
+
+    List<Dish> getDishesByMenuItem(MenuItem menuItem);
+
+    List<Dish> getDishesByOrderAndMenuItem(Order order, MenuItem menuItem);
+
+    @Query("SELECT " +
+            "new com.application.entity.dto.PopularDishReport(d.menuItem.dishName, COUNT (d.dish_id), SUM (d.price.priceValue)) " +
+            "FROM " +
+            "Dish d " +
+            "GROUP BY d.menuItem.dishName")
+    List<PopularDishReport> getSalesByItemMenuAllTime();
+
+    void removeDishByOrderAndMenuItem(Order order, MenuItem menuItem);
+
+}
