df0356279fe1bcb347361e7d748bec743502fecb
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-18 00:24:04 -0400

    Added mapping for Address, Dishes, etc - renamed table as order is a reserved word in MySQL

--- src/main/java/com/application/entity/Order.java
+++ src/main/java/com/application/entity/Order.java
@@ -1,34 +1,55 @@
 package com.application.entity;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private int orderId;
+    @Column(name = "total_amount")
+    private int totalAmount;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany (mappedBy = "order")
+    private List<Dish> orderedDishes;
 
     @ManyToOne
     @JoinColumn(name = "order_status_id")
     private OrderStatus orderStatus;
 
     @Column(name = "ordered_time")
-    private LocalDateTime orderedTime;
+    private Timestamp orderedTime;
+
+    @ManyToOne
+    @JoinColumn(name = "payment_method_id")
+    private PaymentMethod paymentMethod;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+
 
     public Order() {
     }
 
-    public Order(User user, OrderStatus orderStatus, LocalDateTime orderedTime) {
+    public Order(int totalAmount, User user, List<Dish> orderedDishes,
+                 OrderStatus orderStatus, Timestamp orderedTime, PaymentMethod paymentMethod, Address address) {
+        this.totalAmount = totalAmount;
         this.user = user;
+        this.orderedDishes = orderedDishes;
         this.orderStatus = orderStatus;
         this.orderedTime = orderedTime;
+        this.paymentMethod = paymentMethod;
+        this.address = address;
     }
 
     public int getOrderId() {
@@ -55,19 +76,53 @@ public class Order {
         this.orderStatus = orderStatus;
     }
 
-    public LocalDateTime getOrderedTime() {
+    public Timestamp getOrderedTime() {
         return orderedTime;
     }
 
-    public void setOrderedTime(LocalDateTime orderedTime) {
+    public void setOrderedTime(Timestamp orderedTime) {
         this.orderedTime = orderedTime;
+    }
+
+    public List<Dish> getOrderedDishes() {
+        return orderedDishes;
+    }
+
+    public void setOrderedDishes(List<Dish> orderedDishes) {
+        this.orderedDishes = orderedDishes;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     @Override
     public String toString() {
         return "Order{" +
-                "order_id=" + orderId +
+                "orderId=" + orderId +
+                ", totalAmount=" + totalAmount +
                 ", user=" + user +
+                ", orderedDishes=" + orderedDishes +
                 ", orderStatus=" + orderStatus +
                 ", orderedTime=" + orderedTime +
                 '}';
