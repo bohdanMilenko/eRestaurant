d38fe075083bd2852098fcc394d1f9495505b38e
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-17 22:41:45 -0400

    Utility class to facilitate update of statuses for Orders and Dishes

---
+++ src/main/java/com/application/util/StatusUpdate.java
@@ -0,0 +1,27 @@
+package com.application.util;
+
+import com.application.entity.Dish;
+import com.application.entity.DishStatus;
+import com.application.entity.OrderStatus;
+
+public class StatusUpdate {
+
+    public static void updateDishStatus(DishStatus dishStatus) {
+        switch (dishStatus.getDishStatusName()) {
+            case "Waiting" -> dishStatus.setDishStatusName("Pending");
+            case "Pending" -> dishStatus.setDishStatusName("Ready for Delivery");
+            case "Ready for Delivery" -> dishStatus.setDishStatusName("Sent Out");
+            default -> dishStatus.setDishStatusName("Waiting");
+        };
+    }
+
+    public static void updateOrderStatus(OrderStatus orderStatus) {
+        switch (orderStatus.getOrderStatusName()) {
+            case "Pending" -> orderStatus.setOrderStatusName("Ready for Delivery");
+            case "Ready for Delivery" -> orderStatus.setOrderStatusName("Delivering");
+            case "Delivering" -> orderStatus.setOrderStatusName("Delivered");
+            default -> orderStatus.setOrderStatusName("Pending");
+        };
+    }
+
+}
