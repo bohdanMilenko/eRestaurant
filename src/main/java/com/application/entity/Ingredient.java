38d94e477e99e89d6b5f08e30edb7fad7d366913
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-17 22:44:32 -0400

    Removed from mapping for now

--- src/main/java/com/application/entity/Ingredient.java
+++ src/main/java/com/application/entity/Ingredient.java
@@ -1,89 +1,89 @@
-package com.application.entity;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "ingredient")
-public class Ingredient {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ingredient_id")
-    private int ingredientId;
-    @Column(name = "isAllergic")
-    private boolean isAllergic;
-    @Column(name = "isVegetarian")
-    private boolean isVegetarian;
-    @Column(name = "isVegan")
-    private boolean isVegan;
-    @Column(name = "shelf_time")
-    private int shelfTime;
-
-    //Used to be: private List<MenuItemIngredient> menuItems = new ArrayList<>();
-    @OneToMany(mappedBy = "ingredient")
-    private List<MenuItemIngredient> menuItems;
-
-
-    public Ingredient() {
-    }
-
-    public Ingredient(int ingredientId, boolean isAllergic, boolean isVegetarian, boolean isVegan, int shelfTime) {
-        this.ingredientId = ingredientId;
-        this.isAllergic = isAllergic;
-        this.isVegetarian = isVegetarian;
-        this.isVegan = isVegan;
-        this.shelfTime = shelfTime;
-    }
-
-    public int getIngredientId() {
-        return ingredientId;
-    }
-
-    public void setIngredientId(int ingredientId) {
-        this.ingredientId = ingredientId;
-    }
-
-    public boolean isAllergic() {
-        return isAllergic;
-    }
-
-    public void setAllergic(boolean allergic) {
-        isAllergic = allergic;
-    }
-
-    public boolean isVegetarian() {
-        return isVegetarian;
-    }
-
-    public void setVegetarian(boolean vegetarian) {
-        isVegetarian = vegetarian;
-    }
-
-    public boolean isVegan() {
-        return isVegan;
-    }
-
-    public void setVegan(boolean vegan) {
-        isVegan = vegan;
-    }
-
-    public int getShelfTime() {
-        return shelfTime;
-    }
-
-    public void setShelfTime(int shelfTime) {
-        this.shelfTime = shelfTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Ingredient{" +
-                "ingredientId=" + ingredientId +
-                ", isAllergic=" + isAllergic +
-                ", isVegetarian=" + isVegetarian +
-                ", isVegan=" + isVegan +
-                ", shelfTime=" + shelfTime +
-                '}';
-    }
-}
+//package com.application.entity;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "ingredient")
+//public class Ingredient {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "ingredient_id")
+//    private int ingredientId;
+//    @Column(name = "isAllergic")
+//    private boolean isAllergic;
+//    @Column(name = "isVegetarian")
+//    private boolean isVegetarian;
+//    @Column(name = "isVegan")
+//    private boolean isVegan;
+//    @Column(name = "shelf_time")
+//    private int shelfTime;
+//
+//    //Used to be: private List<MenuItemIngredient> menuItems = new ArrayList<>();
+//    @OneToMany(mappedBy = "ingredient")
+//    private List<MenuItemIngredient> menuItems;
+//
+//
+//    public Ingredient() {
+//    }
+//
+//    public Ingredient(int ingredientId, boolean isAllergic, boolean isVegetarian, boolean isVegan, int shelfTime) {
+//        this.ingredientId = ingredientId;
+//        this.isAllergic = isAllergic;
+//        this.isVegetarian = isVegetarian;
+//        this.isVegan = isVegan;
+//        this.shelfTime = shelfTime;
+//    }
+//
+//    public int getIngredientId() {
+//        return ingredientId;
+//    }
+//
+//    public void setIngredientId(int ingredientId) {
+//        this.ingredientId = ingredientId;
+//    }
+//
+//    public boolean isAllergic() {
+//        return isAllergic;
+//    }
+//
+//    public void setAllergic(boolean allergic) {
+//        isAllergic = allergic;
+//    }
+//
+//    public boolean isVegetarian() {
+//        return isVegetarian;
+//    }
+//
+//    public void setVegetarian(boolean vegetarian) {
+//        isVegetarian = vegetarian;
+//    }
+//
+//    public boolean isVegan() {
+//        return isVegan;
+//    }
+//
+//    public void setVegan(boolean vegan) {
+//        isVegan = vegan;
+//    }
+//
+//    public int getShelfTime() {
+//        return shelfTime;
+//    }
+//
+//    public void setShelfTime(int shelfTime) {
+//        this.shelfTime = shelfTime;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Ingredient{" +
+//                "ingredientId=" + ingredientId +
+//                ", isAllergic=" + isAllergic +
+//                ", isVegetarian=" + isVegetarian +
+//                ", isVegan=" + isVegan +
+//                ", shelfTime=" + shelfTime +
+//                '}';
+//    }
+//}
--- src/main/java/com/application/entity/MenuItemIngredient.java
+++ src/main/java/com/application/entity/MenuItemIngredient.java
@@ -1,68 +1,68 @@
-package com.application.entity;
-
-import javax.persistence.*;
-import java.util.Objects;
-
-@Entity
-@Table(name = "menu_item_ingredients")
-public class MenuItemIngredient {
-
-    @EmbeddedId
-    private MenuItemIngredientId menuItemIngredientIdId;
-
-    @ManyToOne
-    @MapsId("menuItemId")
-    private MenuItem menuItem;
-
-    @ManyToOne
-    @MapsId("ingredientId")
-    private Ingredient ingredient;
-
-    public MenuItemIngredient() {
-
-    }
-
-    public MenuItemIngredient(MenuItemIngredientId menuItemIngredientIdId, MenuItem menuItem, Ingredient ingredient) {
-        this.menuItemIngredientIdId = menuItemIngredientIdId;
-        this.menuItem = menuItem;
-        this.ingredient = ingredient;
-    }
-
-    public MenuItemIngredientId getMenuItemIngredientIdId() {
-        return menuItemIngredientIdId;
-    }
-
-    public void setMenuItemIngredientIdId(MenuItemIngredientId menuItemIngredientIdId) {
-        this.menuItemIngredientIdId = menuItemIngredientIdId;
-    }
-
-    public MenuItem getMenuItem() {
-        return menuItem;
-    }
-
-    public void setMenuItem(MenuItem menuItem) {
-        this.menuItem = menuItem;
-    }
-
-    public Ingredient getIngredient() {
-        return ingredient;
-    }
-
-    public void setIngredient(Ingredient ingredient) {
-        this.ingredient = ingredient;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MenuItemIngredient that = (MenuItemIngredient) o;
-        return  menuItem.equals(that.menuItem) &&
-                ingredient.equals(that.ingredient);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(menuItemIngredientIdId, menuItem, ingredient);
-    }
-}
+//package com.application.entity;
+//
+//import javax.persistence.*;
+//import java.util.Objects;
+//
+//@Entity
+//@Table(name = "menu_item_ingredients")
+//public class MenuItemIngredient {
+//
+//    @EmbeddedId
+//    private MenuItemIngredientId menuItemIngredientIdId;
+//
+//    @ManyToOne
+//    @MapsId("menuItemId")
+//    private MenuItem menuItem;
+//
+//    @ManyToOne
+//    @MapsId("ingredientId")
+//    private Ingredient ingredient;
+//
+//    public MenuItemIngredient() {
+//
+//    }
+//
+//    public MenuItemIngredient(MenuItemIngredientId menuItemIngredientIdId, MenuItem menuItem, Ingredient ingredient) {
+//        this.menuItemIngredientIdId = menuItemIngredientIdId;
+//        this.menuItem = menuItem;
+//        this.ingredient = ingredient;
+//    }
+//
+//    public MenuItemIngredientId getMenuItemIngredientIdId() {
+//        return menuItemIngredientIdId;
+//    }
+//
+//    public void setMenuItemIngredientIdId(MenuItemIngredientId menuItemIngredientIdId) {
+//        this.menuItemIngredientIdId = menuItemIngredientIdId;
+//    }
+//
+//    public MenuItem getMenuItem() {
+//        return menuItem;
+//    }
+//
+//    public void setMenuItem(MenuItem menuItem) {
+//        this.menuItem = menuItem;
+//    }
+//
+//    public Ingredient getIngredient() {
+//        return ingredient;
+//    }
+//
+//    public void setIngredient(Ingredient ingredient) {
+//        this.ingredient = ingredient;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        MenuItemIngredient that = (MenuItemIngredient) o;
+//        return  menuItem.equals(that.menuItem) &&
+//                ingredient.equals(that.ingredient);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(menuItemIngredientIdId, menuItem, ingredient);
+//    }
+//}
