b2ebc274c571ddbb4033e853ed2bb2ec3b5ff926
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-22 23:23:41 -0400

    Updated queries with custom SQL statements

--- src/main/java/com/application/repository/IOrderRepo.java
+++ src/main/java/com/application/repository/IOrderRepo.java
@@ -3,13 +3,36 @@ package com.application.repository;
 import com.application.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface IOrderRepo extends JpaRepository<Order, Integer> {
 
-    List<Order> getOrdersByUser_UserId(int userId);
+    @Query("SELECT " +
+                "o " +
+            "FROM " +
+                "Order o " +
+            "LEFT JOIN FETCH " +
+                "o.orderedDishes " +
+            "WHERE " +
+                "o.user.userId = :userId")
+    List<Order> getOrdersByUser_UserId(@Param("userId") int userId);
+
+    //TODO - RETURNS AS MANY ORDERS AS MANY DISHES INSIDE OF THE ORDER
+    @Query("SELECT " +
+                "o " +
+            "FROM " +
+                "Order o " +
+            "LEFT JOIN FETCH " +
+                "o.orderedDishes " +
+            "WHERE " +
+                "o.orderedTime >= :firstDate " +
+            "AND " +
+                "o.orderedTime <= :secondDate")
+    List<Order> getOrdersByOrderedTimeBetween(@Param("firstDate")Timestamp firstDate,@Param("secondDate") Timestamp secondDate);
 
     List<Order> getOrdersByOrderStatus_OrderStatusName(String orderStatus);
 
