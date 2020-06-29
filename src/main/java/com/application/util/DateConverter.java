c6ec0d97b0ebc0cdb887a6c0088cd573efba63d5
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-28 20:12:06 -0400

    Created controller to facilitate LocalDateTime -> Timestamp

---
+++ src/main/java/com/application/util/DateConverter.java
@@ -0,0 +1,20 @@
+package com.application.util;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Converter
+public class DateConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+        return Timestamp.valueOf(localDateTime);
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
+        return timestamp.toLocalDateTime();
+    }
+}
