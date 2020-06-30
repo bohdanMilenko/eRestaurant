4ad792dd9fdd6a8784b805151bdeae33e6a36bcd
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-30 01:00:34 -0400

    Fixed mapping for modelMapper

--- src/main/java/com/application/controller/OrderController.java
+++ src/main/java/com/application/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.application.controller;
 
+import com.application.entity.Address;
 import com.application.entity.Order;
+import com.application.entity.OrderStatus;
 import com.application.entity.User;
 import com.application.entity.dto.OrderDTO;
 import com.application.entity.dto.UserDTO;
@@ -10,6 +12,8 @@ import com.application.service.IUserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.print.attribute.standard.Destination;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,20 +30,36 @@ import java.util.stream.Collectors;
 @RequestMapping("/order")
 public class OrderController {
 
-    @Autowired
+//    @Autowired
     private ModelMapper modelMapper;
 
-    @Autowired
+//    @Autowired
     private IOrderService orderService;
 
-    @Autowired
+//    @Autowired
     private IUserService userService;
-    @Autowired
+//    @Autowired
     private UserController userController;
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-//    @PostMapping("/date")
+
+    public OrderController() {
+        modelMapper.addMappings(orderToDTOMapping);
+        modelMapper.addMappings(DTOToOrderMapping);
+    }
+
+    @Autowired
+    public OrderController(ModelMapper modelMapper, IOrderService orderService, IUserService userService, UserController userController) {
+        this.modelMapper = modelMapper;
+        this.orderService = orderService;
+        this.userService = userService;
+        this.userController = userController;
+        modelMapper.addMappings(orderToDTOMapping);
+        modelMapper.addMappings(DTOToOrderMapping);
+    }
+
+    //    @PostMapping("/date")
 //    public void date(@RequestParam("date")
 //                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date) {
 //        // ...
@@ -47,20 +68,58 @@ public class OrderController {
     @GetMapping
     public ResponseEntity getOrdersForUser(@RequestBody UserDTO userDTO) throws ServiceException, JsonProcessingException {
         List<Order> orderList = orderService.getOrdersByUser(userController.convertToEntity(userDTO));
+        orderList.forEach( s -> System.out.println(s.toString()));
         List<OrderDTO> orderDTOList = orderList.stream()
-                .map(this::convertToDto)
+                .map(order -> modelMapper.map(order, OrderDTO.class))
                 .collect(Collectors.toList());
         return new ResponseEntity(objectMapper.writeValueAsString(orderDTOList), HttpStatus.OK);
     }
 
+    PropertyMap<Order, OrderDTO> orderToDTOMapping = new PropertyMap<Order, OrderDTO>() {
+        protected void configure() {
+            map().setAddressLine(source.getAddress().getAddressLine1());
+            map().setOrderedDateTime(source.getOrderedTime());
+            map().setOrderStatus(source.getOrderStatus().getOrderStatusName());
+            map().setTotalSum(source.getTotalAmount());
+        }
+    };
 
-    OrderDTO convertToDto(Order order) {
-        return modelMapper.map(order, OrderDTO.class);
-    }
+    PropertyMap<OrderDTO, Order> DTOToOrderMapping = new PropertyMap<OrderDTO, Order>() {
+        protected void configure() {
+            map().getAddress().setAddressLine1(source.getAddressLine());
+            map().getOrderStatus().setOrderStatusName((source.getOrderStatus()));
+            map().setTotalAmount(source.getTotalSum());
+            map().setOrderedTime(source.getOrderedDateTime());
+        }
+    };
 
-    Order convertToEntity(OrderDTO orderDTO) {
-        return modelMapper.map(orderDTO, Order.class);
-    }
+
+
+//    OrderDTO convertToDto(Order order) {
+//        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+//        TypeMap<Order, OrderDTO> typeMap = modelMapper.typeMap(Order.class, OrderDTO.class).addMappings(mapper -> {
+//            mapper.map( srcOrder -> srcOrder.getAddress().getAddressLine1(), OrderDTO::setAddressLine);
+////            mapper.map(srcOrder -> srcOrder.getTotalAmount(), OrderDTO::setTotalSum);
+//            mapper.map(srcOrder -> srcOrder.getOrderedTime(), OrderDTO::setOrderedDateTime);
+//            mapper.map( srcOrder -> srcOrder.getOrderStatus().getOrderStatusName(), OrderDTO::setOrderStatus);
+//
+//        });
+//        return typeMap.map(order);
+////        return modelMapper.map(order, OrderDTO.class);
+//    }
+//
+//    Order convertToEntity(OrderDTO orderDTO) {
+//        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+//        TypeMap<OrderDTO, Order> typeMap = modelMapper.typeMap( OrderDTO.class, Order.class).addMappings(mapper -> {
+//            mapper.map( srcOrderDTO -> new Address(srcOrderDTO.getAddressLine()), Order::setAddress);
+//            mapper.map(OrderDTO::getTotalSum, Order::setTotalAmount);
+//            mapper.map(OrderDTO::getOrderedDateTime, Order::setOrderedTime);
+//            mapper.map( srcOrderDTO -> new OrderStatus(srcOrderDTO.getOrderStatus()), Order::setOrderStatus);
+//
+//        });
+//        return typeMap.map(orderDTO);
+////        return modelMapper.map(orderDTO, Order.class);
+//    }
 
 
 }
