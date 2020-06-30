c97c6a4302331868b7d26963507032eefc4b5d34
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-06-29 23:59:23 -0400

    Added logging

--- src/main/java/com/application/entity/dto/OrderDTO.java
+++ src/main/java/com/application/entity/dto/OrderDTO.java
@@ -4,13 +4,13 @@ import com.application.entity.Dish;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -18,15 +18,15 @@ import java.util.List;
 public class OrderDTO {
 
     @NotNull
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
-    private LocalDate orderedDateTime;
+    private LocalDateTime orderedDateTime;
     @NotNull
     private String orderStatus;
     @NotNull
     private int totalSum;
-//    @NotNull
+    //    @NotNull
 //    private List<Dish> orderedDishes;
     @NotNull
     private String addressLine;
@@ -34,7 +34,7 @@ public class OrderDTO {
     public OrderDTO() {
     }
 
-    public OrderDTO(LocalDate orderedDateTime, String orderStatus, int totalSum,
+    public OrderDTO(LocalDateTime orderedDateTime, String orderStatus, int totalSum,
                     List<Dish> orderedDishes, String addressLine) {
         this.orderedDateTime = orderedDateTime;
         this.orderStatus = orderStatus;
@@ -43,4 +43,36 @@ public class OrderDTO {
         this.addressLine = addressLine;
     }
 
+
+    public LocalDateTime getOrderedDateTime() {
+        return orderedDateTime;
+    }
+
+    public void setOrderedDateTime(LocalDateTime orderedDateTime) {
+        this.orderedDateTime = orderedDateTime;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public int getTotalSum() {
+        return totalSum;
+    }
+
+    public void setTotalSum(int totalSum) {
+        this.totalSum = totalSum;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
 }
