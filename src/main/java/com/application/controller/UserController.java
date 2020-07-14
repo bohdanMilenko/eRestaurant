f685bfefe77ac72f439cc83ba441e22e814bdb2b
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-07-13 22:36:02 -0400

    Moved code to OrderController to facilitate the architecture

--- src/main/java/com/application/controller/UserController.java
+++ src/main/java/com/application/controller/UserController.java
@@ -1,50 +1,45 @@
 package com.application.controller;
 
 import com.application.entity.Order;
-import com.application.entity.User;
+import com.application.entity.dto.DishDTO;
 import com.application.entity.dto.OrderDTO;
 import com.application.entity.dto.UserDTO;
 import com.application.exception.ServiceException;
 import com.application.service.IOrderService;
 import com.application.service.IUserService;
+import com.application.util.dtoEntityConverter.DishConverter;
 import com.application.util.dtoEntityConverter.OrderConverter;
-import org.modelmapper.ModelMapper;
+import com.application.util.dtoEntityConverter.UserConverter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
-
-    @Autowired
     private IUserService userService;
-    @Autowired
     private IOrderService orderService;
 
     public UserController() {
     }
 
+    @Autowired
     public UserController(IUserService userService, IOrderService orderService) {
         this.userService = userService;
         this.orderService = orderService;
     }
 
-    @PostMapping(value ="/createOrder" ,consumes = "application/json")
-    public ResponseEntity<HttpStatus> createNewOrder(@RequestBody OrderDTO orderDTO) throws ServiceException {
-        try {
-            orderService.addOrder(OrderConverter.convertDTOToOrder(orderDTO));
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (ServiceException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
 
-    }
+
+
+
 
 
 }
