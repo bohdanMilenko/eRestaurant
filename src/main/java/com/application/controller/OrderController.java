5b02354932ff218ca3ab41f8b24e7a21a3b78977
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-07-13 22:38:16 -0400

    Created new logic to retrieve and write to db, updated resources

--- src/main/java/com/application/controller/OrderController.java
+++ src/main/java/com/application/controller/OrderController.java
@@ -1,27 +1,30 @@
 package com.application.controller;
 
 import com.application.entity.Order;
+import com.application.entity.User;
+import com.application.entity.dto.DishDTO;
 import com.application.entity.dto.OrderDTO;
 import com.application.entity.dto.UserDTO;
 import com.application.exception.ServiceException;
 import com.application.service.IOrderService;
 import com.application.service.IUserService;
+import com.application.util.dtoEntityConverter.DishConverter;
 import com.application.util.dtoEntityConverter.OrderConverter;
 import com.application.util.dtoEntityConverter.UserConverter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
 
@@ -44,40 +47,29 @@ public class OrderController {
 //        // ...
 //    }
 
-    @GetMapping
-    public ResponseEntity<String> getOrdersForUser(@RequestBody UserDTO userDTO) throws ServiceException, JsonProcessingException {
-        List<Order> orderList = orderService.getOrdersByUser(UserConverter.convertToEntity(userDTO));
-        orderList.forEach(s -> System.out.println(s.toString()));
-        List<OrderDTO> orderDTOList = OrderConverter.convertOrderListToDTO(orderList);
-        return new ResponseEntity<>(objectMapper.writeValueAsString(orderDTOList), HttpStatus.OK);
+
+    @GetMapping(value = {"/{email}"})
+    public ResponseEntity<List<OrderDTO>> getOrdersByUser(@PathVariable String email) {
+        try {
+            log.info("Controller tries to getOrdersByUser(String email) where email = {}", email);
+            return new ResponseEntity<>(OrderConverter.convertOrderListToDTO(orderService.getOrdersByUserEmail(email)), HttpStatus.OK);
+        } catch (ServiceException e) {
+            log.info("User Controller is not able to process getOrdersByUser(String email) where email = {} and throws: {}", email, e.toString());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-
-//    OrderDTO convertToDto(Order order) {
-//        modelMapper.getConfiguration().setAmbiguityIgnored(true);
-//        TypeMap<Order, OrderDTO> typeMap = modelMapper.typeMap(Order.class, OrderDTO.class).addMappings(mapper -> {
-//            mapper.map( srcOrder -> srcOrder.getAddress().getAddressLine1(), OrderDTO::setAddressLine);
-////            mapper.map(srcOrder -> srcOrder.getTotalAmount(), OrderDTO::setTotalSum);
-//            mapper.map(srcOrder -> srcOrder.getOrderedTime(), OrderDTO::setOrderedDateTime);
-//            mapper.map( srcOrder -> srcOrder.getOrderStatus().getOrderStatusName(), OrderDTO::setOrderStatus);
-//
-//        });
-//        return typeMap.map(order);
-////        return modelMapper.map(order, OrderDTO.class);
-//    }
-//
-//    Order convertToEntity(OrderDTO orderDTO) {
-//        modelMapper.getConfiguration().setAmbiguityIgnored(true);
-//        TypeMap<OrderDTO, Order> typeMap = modelMapper.typeMap( OrderDTO.class, Order.class).addMappings(mapper -> {
-//            mapper.map( srcOrderDTO -> new Address(srcOrderDTO.getAddressLine()), Order::setAddress);
-//            mapper.map(OrderDTO::getTotalSum, Order::setTotalAmount);
-//            mapper.map(OrderDTO::getOrderedDateTime, Order::setOrderedTime);
-//            mapper.map( srcOrderDTO -> new OrderStatus(srcOrderDTO.getOrderStatus()), Order::setOrderStatus);
-//
-//        });
-//        return typeMap.map(orderDTO);
-////        return modelMapper.map(orderDTO, Order.class);
-//    }
+    @PostMapping(value = "/{email}/newOrder", consumes = "application/json")
+    public ResponseEntity<HttpStatus> createNewOrder(@RequestBody OrderDTO orderDTO, @PathVariable String email) throws ServiceException {
+        try {
+            Order order = OrderConverter.convertDTOToOrder(orderDTO);
+            order.setUser(new User(email));
+            orderService.addOrder(order);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ServiceException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
 }
