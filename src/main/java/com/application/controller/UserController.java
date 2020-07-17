f5401081676bffd02ffe1b7e9b2094daa6be9e81
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-07-16 23:45:55 -0400

    Methods transfer from OrderController

--- src/main/java/com/application/controller/UserController.java
+++ src/main/java/com/application/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.application.controller;
 
+import com.application.entity.Dish;
 import com.application.entity.Order;
+import com.application.entity.User;
 import com.application.entity.dto.DishDTO;
 import com.application.entity.dto.OrderDTO;
 import com.application.entity.dto.UserDTO;
@@ -37,9 +39,29 @@ public class UserController {
     }
 
 
+    @GetMapping(value = {"/{email}"})
+    public ResponseEntity<List<OrderDTO>> getOrdersByUser(@PathVariable String email) {
+        try {
+            log.info("Controller tries to getOrdersByUser(String email) where email = {}", email);
+            return new ResponseEntity<>(OrderConverter.convertOrderListToDTO(orderService.getOrdersByUserEmail(email)), HttpStatus.OK);
+        } catch (ServiceException e) {
+            log.info("User Controller is not able to process getOrdersByUser(String email) where email = {} and throws: {}", email, e.toString());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-
-
+    @PostMapping(value = "/{email}/{address}/newOrder", consumes = "application/json")
+    public ResponseEntity<HttpStatus> createNewOrder(@RequestBody List<DishDTO> dishDTOs, @PathVariable("email") String email, @PathVariable("address") String address) throws ServiceException {
+        try {
+            List<Dish> dishes = DishConverter.convertToEntity(dishDTOs);
+            Order order = new Order();
+            order.setUser(new User(email));
+            orderService.addOrder(order);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ServiceException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
 }
