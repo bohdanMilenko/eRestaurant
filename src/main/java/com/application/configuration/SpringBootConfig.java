e28959f20f9c964e73882f010d3572004af57d2c
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-07-20 23:51:41 -0400

    Moved configs to a proper place

--- src/main/java/com/application/configuration/SpringBootConfig.java
+++ src/main/java/com/application/configuration/SpringBootConfig.java
@@ -1,38 +1,18 @@
-//package com.application.configuration;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@ConfigurationProperties(prefix = "database")
-//public class SpringBootConfig {
-//
-//    String url;
-//    String username;
-//    String password;
-//
-//
-//    public String getUrl() {
-//        return url;
-//    }
-//
-//    public void setUrl(String url) {
-//        this.url = url;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
+package com.application.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBootConfig {
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
